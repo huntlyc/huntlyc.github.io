8e11c84c3498c3cde85190694932d1429df22a47
Huntly Cameron <huntly@hotscot.net>
2015-06-01 17:00:01 +0100

    Theme change

--- js/general.js
+++ js/general.js
@@ -7,5 +7,5 @@ jQuery(document).ready(function(){
 jQuery.fn.parallax = function(strength) {
     var scroll_top = jQuery(window).scrollTop();
     var move_value = Math.round(scroll_top * strength);
-    jQuery(this).css('background-position', '0 ' + parseInt(move_value * -1) + 'px');
+    jQuery(this).css('background-position', '0 ' + parseInt(move_value * -2) + 'px');
 };
