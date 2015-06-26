f96df24a5dae07331b3058a7a24b6a0677195366
Huntly Cameron <huntly@hotscot.net>
2015-06-26 14:27:09 +0100

    Theme updates + blog post link fix

--- js/general.js
+++ js/general.js
@@ -1,7 +1,23 @@
+var isMenuShown = false;
+
 jQuery(document).ready(function(){
     jQuery(window).on('scroll', function() {
-        jQuery('.paralax-img').parallax(0.5);
+        if(jQuery(window).outerWidth() > 400){
+            jQuery('.paralax-img').parallax(0.5);
+        }
     });
+
+    jQuery('.menu-icon').click(function(event){
+        event.preventDefault();
+
+        if(!isMenuShown){ //Show
+            jQuery('.site-nav').animate({'margin-top': 0}, 100);
+        }else{ //Hide
+            jQuery('.site-nav').animate({'margin-top': -34}, 100);
+        }
+
+        isMenuShown = !isMenuShown;
+    })
 });
 
 jQuery.fn.parallax = function(strength) {
