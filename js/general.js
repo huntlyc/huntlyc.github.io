991976282377db908cb4a0fb799776814a5ca9b9
Huntly Cameron <huntly@hotscot.net>
2016-04-25 14:20:10 +0100

    Google analytics link tracking

--- js/general.js
+++ js/general.js
@@ -20,8 +20,22 @@ jQuery(document).ready(function(){
     })
 });
 
+jQuery(window).load(function(){
+    jQuery('a.social-link').click(function(event){
+        ga('send', {
+              hitType: 'event',
+              eventCategory: 'SocialLink',
+              eventAction: jQuery(this).attr('data-network'),
+              eventLabel: 'SocialLink'
+        });
+    }); 
+});
+
 jQuery.fn.parallax = function(strength) {
     var scroll_top = jQuery(window).scrollTop();
     var move_value = Math.round(scroll_top * strength);
     jQuery(this).css('background-position', '0 ' + parseInt(move_value * -2) + 'px');
 };
+
+
+
